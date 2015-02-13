37db6784b68e48f08c5893afce7f405e8ab2dabf
frankgu968 <frank.gu@mail.utoronto.ca>
2015-02-13 17:20:06 -0500

    Touchups for MongoDB interfacing

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
 //Connects to the mongodb database
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://Frank:frankgu968@ds060977.mongolab.com:60977/MongoLab-a');
 
 var db = mongoose.connection;	//Get the connection object
+var EntrySchema = new mongoose.Schema({
+	entryName: String,
+	entryData: String
+});
+var entry = mongoose.model('Entry', EntrySchema);
 
 
-http.createServer(function(req, res) {
-	db.on('error', console.log('Connection Error'));
-	db.once('open', function(callback){
-		console.log('success');
-		var EntrySchema = new Schema({
-			entryName: String,
-			entryData: String
-		});
-
-		var entry = mongoose.model('Entry', EntrySchema);
-
-		var message = new entry({
-			entryName : 'introString',
-			entryData : 'Hello world!'
-		});
-
-		var interest = new entry({
-			entryName : 'interest',
-			entryName : 'Learn cool new things!'
-		});
-
-		message.save(function(err, message){
-			if(err) return console.error(err);
-			console.log(message);
-		});
-
-		interest.save(function(err, interest){
-			if(err) return console.error(err);
-			console.log(interest);
-		});
+db.on('error', console.error.bind(console, 'Connection Error'));
+db.once('open', function(callback){
+	console.log('success');
+	/*
+	var message = new entry({
+		entryName : 'introString',
+		entryData : 'Hello world!'
 	});
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+	var interest = new entry({
+		entryName : 'interest',
+		entryData : 'Learn cool new things!'
+	});
+
+	message.save(function(err, message){
+		if(err) return console.error(err);
+		console.log(message);
+	});
+
+	interest.save(function(err, interest){
+		if(err) return console.error(err);
+		console.log(interest);
+	});
+	*/
+});
+	
+http.createServer(function(req, res) {
+	entry.find({ entryName : 'introString'}, function(err, docs){
+		if(err){console.log("error!")};
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end(docs[0]['entryData']);
+	});
+}).listen(80,'127.0.0.1');
